8f6d87ec7e9f515320e3d46c33c9349652745e80
sonsu95 <leesonsu07@gmail.com>
2025-02-05 23:02:16 +0900

    chore: prettier setting

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+const config = {
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  printWidth: 100,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+};
+
+export default config;
